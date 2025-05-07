dcee60031a067284e6ca71a41b5d0b89e87d2b39
Nabanji <pnabanji@gmail.com>
2025-05-07 21:17:06 +0300

    Add specific program pages and routes

---
+++ src/Pages/EmpowerHerProgram/Empower.jsx
@@ -0,0 +1,14 @@
+import './Empower.css';
+import Navbar from '../../components/Navbar/Navbar';
+
+export default function Empower() {
+    return (
+        <div className="empower">
+            <Navbar />
+            <div className="empower-intro">
+                <h1>Empower Her Program</h1>
+                <p>The Empower Her program is dedicated to promoting dignity, health, and self-confidence in young</p>
+            </div>
+        </div>
+    )
+}
